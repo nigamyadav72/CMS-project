51a0a05732ed970ae2b8cdab59c18f178cbdbc35
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-01-06 09:52:57 +0545

    create operation learned

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const { blogs } = require("./model/index")
 const app = express()
+
 //alternative
 // const app =require("express")()
+app.set("view engine","ejs")
+
+//if undefined error ayo form submit garda bhane yo code use garne
+//yo dui ta code 
+app.use(express.urlencoded({extended : true}))
+// app.use(express.json)
+
+require("./model/index")
 
 
 
@@ -9,12 +19,32 @@ app.get('/',(req,res)=>{
     res.render("home.ejs")
 })
 
-
-app.get('/about',(req,res)=>{
-    res.render("about.ejs")
+app.get('/addblog',(req,res)=>{
+    res.render("addBlog.ejs")
 })
 
+app.post("/addblog",async(req,res)=>{
+    console.log(req.body)
 
+    // const title = req.body.title
+    // const subTitle = req.body.subTitle
+    // const description = req.body.description
+
+
+    //mathi 3 line le gareko kura lai hamle 1 line mai object destructuring bata garna sakxam  
+    const {title,subTitle,description} = req.body
+    // console.log(title,subTitle,description)
+
+    //inserting into the blogs tables
+    //await rakhnu parxa since it takes time to display in the database
+    await blogs.create({
+        title : title,
+        subTitle : subTitle,
+        description : description
+    })
+    res.send("Blog added successfully")
+    
+})
 
 
 const PORT = 3000
---
+++ model/userModel.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("user", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull : false
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull:false
+      },
+      
+    
+    });
+    return User;
+  };
